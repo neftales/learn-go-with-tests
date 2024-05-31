e88f32df98c35530ffae5b4de5a8b1cf07069401
Neftales Antunes <neftales@gmail.com>
2024-05-31 11:59:04 -0300

    Fix countdown tests implementating Sleeper

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -3,7 +3,6 @@ package countdown
 import (
 	"fmt"
 	"io"
-	"time"
 )
 
 const (
@@ -11,10 +10,23 @@ const (
 	finalWord      = "Go!"
 )
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 	}
+
 	fmt.Fprint(out, finalWord)
 }
