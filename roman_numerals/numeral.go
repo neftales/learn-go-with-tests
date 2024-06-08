6eddda0d1eadb9755593e52d6162ff3100dc0c7e
Neftales Antunes <neftales@gmail.com>
2024-06-08 13:47:24 -0300

    convert arabic to roman

--- roman_numerals/numeral.go
+++ roman_numerals/numeral.go
@@ -2,15 +2,35 @@ package romannumerals
 
 import "strings"
 
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func ConvertToRoman(arabic int) string {
 	var result strings.Builder
 
-	if arabic == 4 {
-		return "IV"
-	}
-
-	for i := 0; i < arabic; i++ {
-		result.WriteString("I")
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
+		}
 	}
 
 	return result.String()
