6486384309945045a29a1183c73e119fc46d6b86
Neftales Antunes <neftales@gmail.com>
2024-06-22 10:02:10 -0300

    test for properties of conversion between arabic <=> roman

--- roman_numerals/numeral_test.go
+++ roman_numerals/numeral_test.go
@@ -3,10 +3,11 @@ package romannumerals
 import (
 	"fmt"
 	"testing"
+	"testing/quick"
 )
 
 var cases = []struct {
-	Arabic int
+	Arabic uint16
 	Roman  string
 }{
 	{Arabic: 1, Roman: "I"},
@@ -60,5 +61,24 @@ func TestConvertingToArabic(t *testing.T) {
 				t.Errorf("got %d, want %d", got, test.Arabic)
 			}
 		})
+	}
+}
+
+func TestPropertiesOfConverstion(t *testing.T) {
+	assertion := func(arabic uint16) bool {
+		if arabic > 3999 {
+			return true
+		}
+
+		roman := ConvertToRoman(arabic)
+		fromRoman := ConvertToArabic(roman)
+
+		return fromRoman == arabic
+	}
+
+	if err := quick.Check(assertion, &quick.Config{
+		MaxCount: 1000,
+	}); err != nil {
+		t.Error("failed checks", err)
 	}
 }
